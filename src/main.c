9af085fc1c616b0f7cac7bbc4e86baffe3700034
Nikita Kasinski <96413265+nikita-kasinski@users.noreply.github.com>
2023-03-05 12:09:01 +0000

    debug days_since_1970

--- src/main.c
+++ src/main.c
@@ -7,22 +7,19 @@ written by Kasinski Nikita
 
 int days_since_1970(int dd,  // days
                     int mm,  // month
-                    int yyyy // year) 
-{
+                    int yyyy // year
+) {
   const int SECONDS_IN_DAY = 86400;
-  struct tm date;
-  date.tm_mday = dd;
-  date.tm_mon = mm;
-  date.tm_year = yyyy - 1970;
-  return (long long)mktime(&date) / SECONDS_IN_DAY;
+  struct tm date = {0, 0, 0, dd, mm - 1, yyyy - 1900};
+  time_t rawtime = mktime(&date);
+  return (long long)rawtime / SECONDS_IN_DAY;
 }
 
-int main(void) 
-{ 
-  const char* message = "Enter date in the format: dd mm yyyy";
+int main(void) {
+  const char *message = "Enter date in the format: dd mm yyyy";
   int dd, mm, yyyy;
   printf("%s\n", message);
-  scanf ("%d %d %d", &dd, &mm, &yyyy);
-	printf ("Days since 1st January 1970 %d\n", days_since_1970(dd, mm, yyyy));
-  return 0; 
+  scanf("%d %d %d", &dd, &mm, &yyyy);
+  printf("Days since 1st January 1970 %d\n", days_since_1970(dd, mm, yyyy));
+  return 0;
 }
