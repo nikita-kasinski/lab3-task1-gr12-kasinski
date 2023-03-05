4341007fabdf5f11b07bd3b84de9776be0b28a2d
Nikita Kasinski <96413265+nikita-kasinski@users.noreply.github.com>
2023-03-05 12:14:51 +0000

    implement 2 dates handling in main

--- src/main.c
+++ src/main.c
@@ -16,10 +16,15 @@ int days_since_1970(int dd,  // days
 }
 
 int main(void) {
-  const char *message = "Enter date in the format: dd mm yyyy";
-  int dd, mm, yyyy;
+  const char *message = "Enter 2 dates in the format: dd mm yyyy";
+  int dd1, mm1, yyyy1;
+  int dd2, mm2, yyyy2;
   printf("%s\n", message);
-  scanf("%d %d %d", &dd, &mm, &yyyy);
-  printf("Days since 1st January 1970 %d\n", days_since_1970(dd, mm, yyyy));
+  scanf("%d %d %d", &dd1, &mm1, &yyyy1);
+  scanf("%d %d %d", &dd2, &mm2, &yyyy2);
+  int days_since_1970_date1 = days_since_1970(dd1, mm1, yyyy1);
+  int days_since_1970_date2 = days_since_1970(dd2, mm2, yyyy2);
+  printf("Days since 1st January 1970 of the first date: %d\n", days_since_1970_date1);
+  printf("Days since 1st January 1970 of the second date: %d\n", days_since_1970_date2);
   return 0;
 }
