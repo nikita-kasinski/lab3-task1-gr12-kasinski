14d69d1ab9046c637920f780046ce5cab23d1a23
Nikita Kasinski <96413265+nikita-kasinski@users.noreply.github.com>
2023-03-05 11:55:20 +0000

    implement function days_since_1970

--- src/main.c
+++ src/main.c
@@ -1,20 +1,19 @@
-/* main.c 
+/* main.c
 written by Kasinski Nikita
 */
 
 #include <stdio.h>
+#include <time.h>
 
-int days_since_1970(
-  int dd, // days
-  int mm, // month
-  int yyyy // year
-)
-{
-  return 0;
+int days_since_1970(int dd,  // days
+                    int mm,  // month
+                    int yyyy // year
+) {
+  const int SECONDS_IN_DAY = 86400;
+  struct tm date;
+  date.tm_mday = dd;
+  date.tm_mon = mm;
+  date.tm_year = yyyy - 1970;
+  return (long long)mktime(&date) / SECONDS_IN_DAY;
 }
-int main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
-  return 0;
-}
+int main(void) { return 0; }
