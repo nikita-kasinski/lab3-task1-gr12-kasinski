6419e608c4777e8c4be3d8fc4100473ee6b2d1ca
Nikita Kasinski <96413265+nikita-kasinski@users.noreply.github.com>
2023-03-05 11:39:46 +0000

    declare days_since_1970 function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
-/* hello.c */
+/* main.c 
+written by Kasinski Nikita
+*/
+
 #include <stdio.h>
 
+int days_since_1970(
+  int dd, // days
+  int mm, // month
+  int yyyy // year
+)
+{
+  return 0;
+}
 int main (void)
 {
 	printf ("Hello World\n");
